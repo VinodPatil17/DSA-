2db30f90053af2b6d7179a04f252a7e2bfff0d4d
VinodPatil17 <brndxdvl@gmail.com>
2026-09-10 23:53:45 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (72.59%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -17,10 +17,10 @@ public class Solution {
         while(fast != null && fast.next != null){
             fast = fast.next.next;
             slow = slow.next;
+
             if(fast == slow){
                 return true;
             }
-            
         }
         return false;
     }
